019ce569212467582aa81033412b426fe7fa6e56
aleganza <ganzarolli.alessio@gmail.com>
2023-08-16 16:23:52 +0200

    remove all

--- Assets/Scripts/EnemyGFX.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Pathfinding;
-
-public class EnemyGFX : MonoBehaviour
-{
-    
-}
--- Assets/Scripts/VectorValue.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "VectorValue", menuName = "ScriptableObjects/VectorValue", order = 0)]
-public class VectorValue : ScriptableObject
-{
-    public Vector2 initialValue;
-}
